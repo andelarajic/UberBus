b7a25d95dc21ec944cba152d6d90a9d1b8fc039f
andjelarajic <andjelarajic1@gmail.com>
2021-10-05 00:22:58 +0200

    changed navbar

--- src/DestinationGPS/Destination.js
+++ src/DestinationGPS/Destination.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "../Navigation/Navigation";
 import GoogleMapReact from "google-map-react";
+import "./Destination.css"
 
-const Destination = ({ text }) => <div>{text}</div>;
+const Destination = ({text}) => <div>{text}</div>;
 
-export default function SimpleMap() {
+export default function SimpleMap(props) {
+
   const defaultProps = {
     center: {
       lat: 59.329323,
@@ -16,15 +18,21 @@ export default function SimpleMap() {
   return (
     // Important! Always set the container height explicitly
 
-    <div style={{ height: "50vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyATwgpzSq0TU95Mcj8HipmTNUxkwLUEId8" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <Navigation />
-        <Destination lat={59.955413} lng={30.337844} text="My Marker" />
-      </GoogleMapReact>
+    <div style={{ position: "relative"}}>
+      
+      <div style={{ height: "50vh", width: "100%", position:"absolute", zIndex: 0}}>
+        <GoogleMapReact 
+          bootstrapURLKeys={{ key: "AIzaSyATwgpzSq0TU95Mcj8HipmTNUxkwLUEId8" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}>
+          <Destination lat={59.955413} lng={30.337844} text="My Marker" />
+        </GoogleMapReact>
+      </div>
+      
+      <div style={{ position:"absolute", margin: "25px", zIndex: 1}}>
+        <Navigation/>
+      </div>
+      
     </div>
   );
 }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,14 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Navigation.css";
 
-const Navigation = () => {
+const Navigation = (props) => {
   const [showMenu, setShowMenu] = useState(false);
 
   let menu;
 
   if (showMenu) {
     menu = (
-      <div className="nav">
+      <div>
         <div>Contact Support</div>
         <div>Settings</div>
         <div>Information Center</div>
@@ -20,8 +20,8 @@ const Navigation = () => {
 
   return (
     <nav>
-      <div className="text">
-        <FontAwesomeIcon
+      <div>
+        <FontAwesomeIcon 
           icon={faBars}
           onClick={(e) => setShowMenu(!showMenu)}
         />
