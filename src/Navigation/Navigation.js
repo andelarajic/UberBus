e42561fd08706af522253bf7050fe7577e61da6b
andjelarajic <andjelarajic1@gmail.com>
2021-10-06 13:00:33 +0200

    navbar responsiv

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,8 +3,8 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FiMenu } from "react-icons/fi";
 import { BsX } from "react-icons/bs";
-import { FiSettings} from "react-icons/fi";
-import { AiOutlineInfoCircle} from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 import { CgSupport } from "react-icons/cg";
 import "./Navigation.css";
 
@@ -16,30 +16,40 @@ const Navigation = (props) => {
   if (showMenu) {
     menu = (
       <div className="navbar">
-          <BsX className="icon2"
+        <BsX
+          className="icon2"
           icon={faTimes}
           onClick={(e) => setShowMenu(!showMenu)}
         />
         <div className="nav">
-        <button className="navicon"><CgSupport/></button><button className="navtext">Contact Support</button></div>
-        <div className="nav">
-        <button className="navicon"><FiSettings/></button><button className="navtext">Settings</button></div>
-        <div className="nav">
-        <button className="navicon"><AiOutlineInfoCircle/></button><button className="navtext">Information Center</button></div>
-        <div className="nav">
-        <button className="logout">Logout</button>
+          <button className="navicon">
+            <CgSupport />
+          </button>
+          <button className="navtext">Contact Support</button>
         </div>
-</div>
+        <div className="nav">
+          <button className="navicon">
+            <FiSettings />
+          </button>
+          <button className="navtext">Settings</button>
+        </div>
+        <div className="nav">
+          <button className="navicon">
+            <AiOutlineInfoCircle />
+          </button>
+          <button className="navtext">Information Center</button>
+        </div>
+        <div className="nav">
+          <button className="logout">Logout</button>
+        </div>
+      </div>
     );
   }
 
   return (
     <nav>
       <div className="icon">
-        <FiMenu
-          icon={faBars}
-          onClick={(e) => setShowMenu(!showMenu)}
-        />
+        <FiMenu icon={faBars} onClick={(e) => setShowMenu(!showMenu)} />
       </div>
       {menu}
     </nav>
