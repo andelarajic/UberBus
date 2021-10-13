67408679ff34c4d8aca81a266c061a787bfd3990
andjelarajic <andjelarajic1@gmail.com>
2021-10-13 23:41:01 +0200

    added style in DestinationMap

--- src/DestinationMap/DestinationMap.js
+++ src/DestinationMap/DestinationMap.js
@@ -4,27 +4,37 @@ import "./DestinationMap.css";
 
 function DestinationMap(props) {
   return (
-    <div classname="UberMap">
-      <InfoMap />
-      <div className="DestinationText">
-        <p>Pickup</p>
-        <p>Location</p>
-        <p>Dropp-off</p>
-        <p>Location</p>
-      </div>
+    <div>
       <div>
-        <img className="UberImg" src="Images/Badge.png" alt="" />
-        <hr className="vl" />
-        <img className="UberImg" src="Images/Badge2.png" alt="" />
+        <InfoMap />
+        <div style={{display: "flex", flexDirection: 'row'}}>
+          <div style={{display: "flex", flexDirection: 'column', marginTop: "10px", height: "25vh"}}>
+            <img className="top" src="Images/Badge.jpeg" alt="" />
+            <hr className="line" />
+            <img className="bottom" src="Images/Badge2.jpeg" alt="" />
+          </div>
+          <div>
+            <div style={{marginBottom: "50px"}}>
+              <p className="infoText">Pickup</p>
+              <p className="stationText">Odenplan</p>
+            </div>
+            <div>
+              <p className="infoText">Dropp-off</p>
+              <p className="stationText">Mall of Scandinavia</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <button
-        className="ButtonDestination"
-        onClick={() => {
-          props.history.push("/destination");
-        }}
-      >
-        Add Payment Method
-      </button>
+      <div style={{paddingBottom: "20px"}}>
+          <button
+              className="ButtonDestination"
+              onClick={() => {
+                props.history.push("/destination");
+              }}
+            >
+              Add Payment Method
+            </button>
+        </div>
     </div>
   );
 }
